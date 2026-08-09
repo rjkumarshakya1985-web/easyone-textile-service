bf105f32ca1f2730dd40608b63d15a20e794e720
Mehtab <mehtabali@live.com>
2026-08-09 11:12:10 +0530

    Deployment version introduced

---
+++ EasyOneService/Controllers/DeploymentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyOne.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeploymentController : ControllerBase
+    {
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            return Ok(new
+            {
+                version = "1.0",
+                message = "Deployment Slot Demo"
+            });
+        }
+    }
+}
